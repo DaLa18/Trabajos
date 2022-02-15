cfd74c91c60af64201242b49ba246bcc9fe16e12
DaLa18 <al02920466@tecmilenio.mx>
2022-02-15 01:26:56 -0600

    Se logro

---
+++ Operaciones.java
@@ -0,0 +1,39 @@
+public class Operaciones {
+
+    public static void main(String[] args){
+        int num1 = 14;
+        String valorx = "X:";
+        System.out.println(valorx , num1);
+
+        int num2 = 27;
+        String valory = "Y:";
+        System.out.println(valory , num2);
+
+        //Suma
+        int suma = num1 + num2;
+        String sumita = "Suma:";
+        System.out.println(sumita , suma);
+
+        // Resta
+        int resta = num1 - num2;
+        String resti = "Resta:";
+        System.out.println(resti , resta);
+
+        //Multiplicación
+        double multiplicacion = num1 * num2;
+        String multi = "Multiplicación:";
+        System.out.println(multi , multiplicacion);
+
+        //División
+        double division = num1 / num2;
+        String divi = "Suma:";
+        System.out.println(divi , division);
+
+        //Modúlo
+        double modulo = num1 % num2;
+        String modu = "Módulo:";
+        System.out.println(modu , modulo);
+    }
+ 
+
+}
