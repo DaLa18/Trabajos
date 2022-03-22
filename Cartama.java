4b3aca7575dc262c4c1f10286dda5a99f2a2c81e
Dana <78758781+DaLa18@users.noreply.github.com>
2022-03-21 21:58:12 -0600

    Delete Cartama.java

--- Cartama.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class Cartama {
-
-    ArrayList<String> deck = new ArrayList<String>();
-    static String[] elsimbolito = { "Trebol", "Pica", "Corazon", "Diamante" };
-    static String[] elcolor = { "Rojo", "Negro" };
-    static String[] elnumerito = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "J", "Q", "K" };
-}
